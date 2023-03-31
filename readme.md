bb28bb2a2850fa4859efc91f9d8389d93841edd9
sidiraos <raossidi@gmail.com>
2023-03-31 21:29:47 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+<h1>Audio Player</h1>
+
+<ul>
+    <b style = "color : red ; font-size : 20px;">V1.0</b>
+    <li>basic design</li>
+</ul>
---
+++ script.js
@@ -0,0 +1,11 @@
+const musicsData = [
+  { title: "Solar", artist: "Betical", id: 1 },
+  { title: "Electric-Feel", artist: "TEEMID", id: 2 },
+  { title: "Aurora", artist: "SLUMB", id: 3 },
+  { title: "Lost-Colours", artist: "Fakear", id: 4 },
+];
+
+const playBtn= document.querySelector('#play');
+const nextBtn = document.querySelector('#next');
+const prevBtn = document.querySelector('#prev');
+const shuffleBtn = document.querySelector('#shuffle');
